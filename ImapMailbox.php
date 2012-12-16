78a5e4719352209043369b91990eaca1862f106f
temistokles <j.patus@gmail.com>
2012-12-16 01:46:02 +0100

    Update ImapMailbox.php

--- ImapMailbox.php
+++ ImapMailbox.php
@@ -156,7 +156,7 @@ class ImapMailbox {
 		elseif($partStruct->encoding == 4) {
 			$data = $this->imap_qprint($data);
 		}
-		$data = trim($data);
+		//$data = trim($data); //this damages zip attachments
 
 		$params = array();
 		if(!empty($partStruct->parameters)) {
