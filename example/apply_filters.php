e8bc95f44cb3d85132f56698517df2edbd91e8b9
Ayman Bedair <aymanrb@gmail.com>
2013-11-26 12:40:31 +0200

    fixed few minor issues with the example file

--- example/apply_filters.php
+++ example/apply_filters.php
@@ -7,7 +7,7 @@ define('GMAIL_EMAIL', 'some@gmail.com');
 define('GMAIL_PASSWORD', '*********');
 define('ATTACHMENTS_DIR', dirname(__FILE__) . '/attachments');
 
-$mailbox = new ImapMailbox('{imap.gmail.com:993/imap/ssl}Test Follow Up', GMAIL_EMAIL, GMAIL_PASSWORD, ATTACHMENTS_DIR, 'utf-8');
+$mailbox = new ImapMailbox('{imap.gmail.com:993/imap/ssl}INBOX', GMAIL_EMAIL, GMAIL_PASSWORD, ATTACHMENTS_DIR, 'utf-8');
 $mails = array();
 
 // Get some mails
@@ -21,27 +21,30 @@ $mailFilters = new MailFilters($mailbox);
 $mailFilters->enableDebug(); //Should be removed in production environment
 
 /** 
-	* ===========================================================
-	* ===============  Add Few Filters Here  ====================
-	* ===========================================================
+* ===========================================================
+* ===============  Add Few Filters Here  ====================
+* ===========================================================
 */
-//Add a filter to check if the message was sent to "mail1@gmail.com" OR "mail2@gmail.com" move it to "New Mail Folder" mailbox
-$mailFilters->addFilter('to',array("mail1@gmail.com","mail2@gmail.com"),array("do"=>"move","params"=>"New Mail Folder"));
+		//Add a filter to check if the message was sent to "mail1@gmail.com" OR "mail2@gmail.com" move it to "New Mail Folder" mailbox
+		$mailFilters->addFilter('to',array("mail1@gmail.com","mail2@gmail.com"),array("do"=>"move","params"=>"New Mail Folder"));
+		
+		//Add a Filter so that if the subject has the words "HasSomethingInIt" return me an array with an element saying "Hello World, the Subject filter was matched"
+		$mailFilters->addFilter('subject',"*RID*",array("do"=>"return","params"=>array("Hello World, the Subject filter was matched")));
+		
+		//Add a Filter that will delete all messages send from "mail1@gmail.com"
+		$mailFilters->addFilter('from',"mail1@gmail.com",array("do"=>"delete"));
 
-//Add a Filter so that if the subject has the words "HasSomethingInIt" return me an array with an element saying "Hello World, the Subject filter was matched"
-$mailFilters->addFilter('subject',"*HasSomethingInIt*",array("do"=>"return","params"=>array("Hello World, the Subject filter was matched")));
-
-//Add a Filter that will delete all messages send from "mail1@gmail.com"
-$mailFilters->addFilter('from',"mail1@gmail.com",array("do"=>"delete"));
-
-/** 
-	* ===============+++  End Add Filters  +++====================
+/*
+* ===============+++  End Add Filters  +++====================
 */
 
 //Loop through all fetched messages and apply the filters to it
 foreach($mailsIds as $i=>$uid){
 	$mail = $mailbox->getMail($uid);
+	
+	//This variable ($appliedFiltersVars) will hold the return params incase the user provided any
 	$appliedFiltersVars = $mailFilters->applyFilters($mail);
+	var_dump($appliedFiltersVars);
 	
 	//Limit to the first 6 messages for the purpose of the example only
 	if($i > 5){
