f0e52f5264f53b73c4571934670fffedefb507c5
barbushin <barbushin@gmail.com>
2015-04-20 16:05:28 +0800

    IncomingMail class moved to separate file

--- src/ImapMailbox.php
+++ src/ImapMailbox.php
@@ -3,7 +3,6 @@
 /**
  * @see https://github.com/barbushin/php-imap
  * @author Barbushin Sergey http://linkedin.com/in/barbushin
- *
  */
 class ImapMailbox {
 
@@ -590,61 +589,6 @@ class ImapMailbox {
 	public function __destruct() {
 		$this->disconnect();
 	}
-}
-
-class IncomingMail {
-
-	public $id;
-	public $date;
-	public $subject;
-
-	public $fromName;
-	public $fromAddress;
-
-	public $to = array();
-	public $toString;
-	public $cc = array();
-	public $replyTo = array();
-
-	public $textPlain;
-	public $textHtml;
-	/** @var IncomingMailAttachment[] */
-	protected $attachments = array();
-
-	public function addAttachment(IncomingMailAttachment $attachment) {
-		$this->attachments[$attachment->id] = $attachment;
-	}
-
-	/**
-	 * @return IncomingMailAttachment[]
-	 */
-	public function getAttachments() {
-		return $this->attachments;
-	}
-
-	/**
-	 * Get array of internal HTML links placeholders
-	 * @return array attachmentId => link placeholder
-	 */
-	public function getInternalLinksPlaceholders() {
-		return preg_match_all('/=["\'](ci?d:(\w+))["\']/i', $this->textHtml, $matches) ? array_combine($matches[2], $matches[1]) : array();
-	}
-
-	public function replaceInternalLinks($baseUri) {
-		$baseUri = rtrim($baseUri, '\\/') . '/';
-		$fetchedHtml = $this->textHtml;
-		foreach($this->getInternalLinksPlaceholders() as $attachmentId => $placeholder) {
-			$fetchedHtml = str_replace($placeholder, $baseUri . basename($this->attachments[$attachmentId]->filePath), $fetchedHtml);
-		}
-		return $fetchedHtml;
-	}
-}
-
-class IncomingMailAttachment {
-
-	public $id;
-	public $name;
-	public $filePath;
 }
 
 class ImapMailboxException extends Exception {
