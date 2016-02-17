db1bb2adabf40421f938c6a034b85e794d8355d2
Thomas Landauer <thomas@landauer.at>
2016-02-17 23:50:18 +0100

    Added PHPDoc to constructor

--- src/PhpImap/Mailbox.php
+++ src/PhpImap/Mailbox.php
@@ -19,6 +19,14 @@ class Mailbox {
 	protected $expungeOnDisconnect = true;
 	private $imapStream;
 
+	/**
+         * @param type $imapPath
+         * @param type $login
+         * @param type $password
+         * @param type $attachmentsDir
+         * @param type $serverEncoding
+         * @throws Exception
+         */
 	public function __construct($imapPath, $login, $password, $attachmentsDir = null, $serverEncoding = 'UTF-8') {
 		$this->imapPath = $imapPath;
 		$this->imapLogin = $login;
