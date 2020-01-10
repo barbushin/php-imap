a92993b49cf6833b062663423538d5dd258959c7
Marv <marv@bapc.ltd>
2020-01-10 16:39:01 +0000

    remove redundant cases

--- src/PhpImap/DataPartInfo.php
+++ src/PhpImap/DataPartInfo.php
@@ -85,10 +85,6 @@ class DataPartInfo
     protected function processFetch()
     {
         switch ($this->encoding) {
-            case ENC7BIT:
-                /** @var string|null */
-                $this->data = $this->data;
-                break;
             case ENC8BIT:
                 $this->data = imap_utf8($this->data);
                 break;
@@ -101,14 +97,6 @@ class DataPartInfo
                 break;
             case ENCQUOTEDPRINTABLE:
                 $this->data = quoted_printable_decode($this->data);
-                break;
-            case ENCOTHER:
-                /** @var string|null */
-                $this->data = $this->data;
-                break;
-            default:
-                /** @var string|null */
-                $this->data = $this->data;
                 break;
         }
 
